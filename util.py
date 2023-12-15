7710503081dc0794a879c5c3e831bf764db61b96
lydie12 <153742993+lydie12@users.noreply.github.com>
2023-12-15 01:03:43 +0000

    Update util.py

--- util.py
+++ util.py
@@ -10,7 +10,7 @@ def config():
         background_image = """
         <style>
         [data-testid="stAppViewContainer"] > .main {
-            background-image: url("");
+            background-image: url("https://img.freepik.com/vecteurs-libre/design-fond-abstrait-technologie-donnees-volumineuses_1017-22911.jpg?w=826&t=st=1702595736~exp=1702596336~hmac=e6ec68887211c57605a3f4314790d4e083211903430fb21566465b3c67bbaba9");
             background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
             background-position: center;  
             background-repeat: no-repeat;
